05cefdee659f8352f76f4fae9afc54786145a2be
Wawan Sugiri <wsugiri@gmail.com>
2023-07-18 10:56:23 +0700

    clean up

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express();
 
 app.use('/', require('./src/routes/view'));
 app.use('/api', require('./src/routes/api'));
-// app.get('/about', (req, res) => res.send('about'));
-// app.get('/login', (req, res) => res.send('login'));
-// app.get('/api/v1', (req, res) => res.send({ message: 'api version 1' }));
 
 const port = process.env.PORT || 3000;
 
